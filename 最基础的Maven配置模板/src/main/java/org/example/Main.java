eb5f215148b8bdb5f67b9292749ca66d7f61b156
Zshuangshuang <1434278632@qq.com>
2021-01-17 20:19:43 +0800

    提交代码

---
+++ 最基础的Maven配置模板/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+
+/**
+ * Author:ZouDouble
+ * Description:
+ * 天气：晴天
+ * 目标：Good Offer
+ * Date    2021-01-17 18:19
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
