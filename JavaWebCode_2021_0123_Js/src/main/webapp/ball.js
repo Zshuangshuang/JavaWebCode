16cf86121cc08bb31b15a2e2ee0270ab852bc5c9
Zshuangshuang <19990306Zss>
2021-01-24 22:19:00 +0800

    提交代码

--- JavaWebCode_2021_0123_Js/src/main/webapp/ball.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    setInterval(function () {
-        let ball = $(".ball");
-        let top = parseInt(ball.css("top").slice(0,-2));
-        top += 10;
-        ball.css("top",top+"px");
-    },30);
-});
